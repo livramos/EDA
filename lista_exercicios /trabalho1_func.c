5944eb13ce50f812b4159da2cc37751cdcba36c8
Liv_Ramos <84927414+livramos@users.noreply.github.com>
2025-09-24 17:40:48 +0000

    cria_)fila

--- lista_exercicios /trabalho1_func.c
+++ lista_exercicios /trabalho1_func.c
@@ -11,12 +11,16 @@ typedef struct  paciente Paciente;
 struct fila{
     int inicial;
     int final;
-    float vet[];
+    float vet[n];
 };
 typedef struct fila Fila;
 
 Fila*fila_cria(void){
-    
+    Fila * f=(Fila)malloc(sizeof(Fila));
+    f->n=0;
+    f->ini=0;
+    return f;
 }
+
 
 
