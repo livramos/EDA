d03059206461db00a7c9103f496c48a73f940141
Liv_Ramos <84927414+livramos@users.noreply.github.com>
2025-09-24 17:35:53 +0000

    trabalho 1_>começado

---
+++ lista_exercicios /trabalho1_func.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct paciente{
+    int index;
+    string cor;
+    p* prox;
+}; 
+typedef struct  paciente Paciente;
+
+struct fila{
+    int inicial;
+    int final;
+    float vet[];
+};
+typedef struct fila Fila;
+
+Fila*fila_cria(void){
+    
+}
+
+
